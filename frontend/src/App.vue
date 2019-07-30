4c93846f5b7a2462b9c8d115821001dfff948af1
sheyguys <sameyu.ping@gmail.com>
2019-07-30 16:18:05 +0700

    update frontend vue

---
+++ frontend/src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="app">
+    <Register />
+  </div>
+</template>
+
+<script>
+import Register from './components/Register.vue'
+export default {
+  name: 'app',
+  components: {
+    Register
+  }
+}
+</script>
